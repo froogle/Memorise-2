7f7af89610599ac9a1385194b05ddb0fa553a41a
Pete Wright <peterjwright@gmail.com>
2021-10-17 09:38:56 +0100

    Initial Commit

---
+++ Memorise 2/Memorise_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorise_2App.swift
+//  Memorise 2
+//
+//  Created by Peter Wright on 17/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Memorise_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
